916546cfccfd445e27ce56d486ef52da8dbca878
OreChou <zhouaoli@nowcoder.com>
2022-04-16 21:29:03 +0800

    feat(Basic): #3 Learn Pickers

---
+++ PickerDemo/PickerDemo/PickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerDemoApp.swift
+//  PickerDemo
+//
+//  Created by OreChou on 2022/4/16.
+//
+
+import SwiftUI
+
+@main
+struct PickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
